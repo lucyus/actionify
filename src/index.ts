d6ec57d2a55fceff3ce3b00415ece9d5d1d0f382
iSwitchL <55490406+iSwitchL@users.noreply.github.com>
2025-02-06 23:41:02 +0100

    feat(@core): implement optional delay and motion settings for 'window.move()'

--- src/index.ts
+++ src/index.ts
@@ -2311,11 +2311,130 @@ export const window = {
           }
           return focusWindow(this.id);
         },
-        move(x?: number, y?: number) {
+        move(x?: number, y?: number, options?: { steps?: number | "auto", delay?: number, motion?: "linear" | "arc" | "wave", curvinessFactor?: number, mirror?: boolean, frequency?: number | "auto" }) {
           if (this.isMinimized) {
             this.restore();
           }
-          return setWindowPosition(this.id, x ?? this.position.x, y ?? this.position.y);
+          const steps = options?.steps === "auto" ? Infinity : Math.max(0, Math.round(options?.steps ?? 0));
+          const delay = Math.max(0, Math.floor(options?.delay ?? 0));
+          const curvinessFactor = options?.curvinessFactor !== undefined ? Math.max(0, Math.min(1, options.curvinessFactor)) : 0.1618;
+          const mirror = options?.mirror ?? false;
+          const motion = options?.motion ?? "linear";
+          const initialX = this.position.x;
+          const initialY = this.position.y;
+          const newX = x ?? initialX;
+          const newY = y ?? initialY;
+          if (steps === 0 || delay === 0) {
+            return time.waitAsync(delay, () => setWindowPosition(this.id, newX, newY));
+          }
+          //Calculate the line from start to end (the shortest diagonal)
+          const dx = newX - initialX;
+          const dy = newY - initialY;
+          // Chebyshev distance
+          const intermediatePositions = Math.max(0, Math.max(Math.abs(dx), Math.abs(dy)) - 1);
+          const possibleSteps = Math.min(steps, intermediatePositions);
+          const preciseDelayPerPosition = delay / (possibleSteps + 1);
+          const delayPerPosition = Math.floor(preciseDelayPerPosition);
+          let accumulatedDelay = 0;
+          const promises: Promise<void>[] = [];
+          if (possibleSteps > 0) {
+            const correctionDelayOccurrence = preciseDelayPerPosition !== delayPerPosition ? Math.ceil(1 / (preciseDelayPerPosition - delayPerPosition)) : Infinity;
+            const directionX = Math.sign(dx);
+            const directionY = Math.sign(dy);
+            const stepX = dx / (possibleSteps + 1);
+            const stepY = dy / (possibleSteps + 1);
+            switch (motion) {
+              case "linear": {
+                for (let offset = 1; offset < possibleSteps + 1; offset++) {
+                  const intermediateX = directionX !== 0 ? Math.round(initialX + offset * stepX) : newX;
+                  const intermediateY = directionY !== 0 ? Math.round(initialY + offset * stepY) : newY;
+                  const correctedDelayPerPosition = delayPerPosition + (offset % correctionDelayOccurrence === 0 ? 1 : 0);
+                  promises.push(time.waitAsync(correctedDelayPerPosition + accumulatedDelay, () => setWindowPosition(this.id, intermediateX, intermediateY)));
+                  accumulatedDelay += correctedDelayPerPosition;
+                }
+                break;
+              }
+              case "arc": {
+                // Bézier curve
+                const midpointX = (initialX + newX) / 2;
+                const midpointY = (initialY + newY) / 2;
+                const distance = Math.sqrt(dx * dx + dy * dy);
+                const perpendicularX = -dy / distance;
+                const perpendicularY = dx / distance;
+                const direction = mirror ? -1 : 1;
+                const controlX = midpointX + curvinessFactor * distance * perpendicularX * direction;
+                const controlY = midpointY + curvinessFactor * distance * perpendicularY * direction;
+                for (let offset = 1; offset < possibleSteps + 1; offset++) {
+                  const t = offset / (possibleSteps + 1);
+                  let intermediateX = Math.round((1 - t) * (1 - t) * initialX + 2 * (1 - t) * t * controlX + t * t * newX);
+                  let intermediateY = Math.round((1 - t) * (1 - t) * initialY + 2 * (1 - t) * t * controlY + t * t * newY);
+                  const correctedDelayPerPosition = delayPerPosition + (offset % correctionDelayOccurrence === 0 ? 1 : 0);
+                  promises.push(time.waitAsync(correctedDelayPerPosition + accumulatedDelay, () => setWindowPosition(this.id, intermediateX, intermediateY)));
+                  accumulatedDelay += correctedDelayPerPosition;
+                }
+                break;
+              }
+              case "wave": {
+                const rawMaxFrequency = Math.floor((possibleSteps + 1) / 30 / 2);
+                const maxFrequency = rawMaxFrequency % 2 === 0 ? rawMaxFrequency : rawMaxFrequency - 1;
+                const frequency = options?.frequency !== undefined ? Math.max(2, Math.min(maxFrequency, options.frequency === "auto" ? maxFrequency : Math.round(options.frequency * 2))) : 2;
+                const distance = Math.sqrt(dx * dx + dy * dy);
+                const halfDistance = distance / 2;
+                const amplitude = curvinessFactor * halfDistance;
+                const mirrorDirection = mirror ? -1 : 1;
+                const directionX = dx / distance;
+                const directionY = dy / distance;
+                for (let offset = 1; offset < possibleSteps + 1; offset++) {
+                  const t = offset / (possibleSteps + 1);
+                  const sineWaveOffset = Math.sin(t * Math.PI * frequency) * amplitude * mirrorDirection;
+                  const intermediateX = Math.round(initialX + t * dx + directionY * sineWaveOffset);
+                  const intermediateY = Math.round(initialY + t * dy - directionX * sineWaveOffset);
+                  const correctedDelayPerPosition = delayPerPosition + (offset % correctionDelayOccurrence === 0 ? 1 : 0);
+                  promises.push(time.waitAsync(correctedDelayPerPosition + accumulatedDelay, () => setWindowPosition(this.id, intermediateX, intermediateY)));
+                  accumulatedDelay += correctedDelayPerPosition;
+                }
+                break;
+              }
+              /* case "circle": {
+                // Determine radius using Euclidean distance
+                const diameter = Math.sqrt(dx * dx + dy * dy);
+                const radius = Math.round(diameter / 2);
+
+                // Determine midpoint
+                const midpointX = (initialX + newX) / 2;
+                const midpointY = (initialY + newY) / 2;
+
+                // Calculate the angle increment for each step
+                const startAngle = Math.atan2(initialY - midpointY, initialX - midpointX); // Angle of initial point
+                const endAngle = Math.atan2(newY - midpointY, newX - midpointX); // Angle of final point
+
+                const direction = mirror ? -1 : 1;
+
+                // Half-circle movement implies that the arc is in a 180-degree range
+                const angleDifference = (endAngle - startAngle) * direction;
+
+                // Ensure the angle difference is between -π and π (normalize angle range)
+                const angleStep = angleDifference / (possibleSteps + 1);
+
+                for (let offset = 1; offset < possibleSteps + 1; offset++) {
+                  const angle = startAngle + offset * angleStep;
+                  // Calculate intermediate point on the arc
+                  const intermediateX = Math.round(midpointX + radius * Math.cos(angle));
+                  const intermediateY = Math.round(midpointY + radius * Math.sin(angle));
+
+                  // Set the cursor position at computed position and at given delay
+                  const correctedDelayPerPosition = delayPerPosition + (offset % correctionDelayOccurrence === 0 ? 1 : 0);
+                  promises.push(time.waitAsync(correctedDelayPerPosition + accumulatedDelay, () => setWindowPosition(this.id, intermediateX, intermediateY)));
+                  accumulatedDelay += correctedDelayPerPosition;
+                }
+                break;
+              } */
+              default:
+                break;
+            }
+          }
+          promises.push(time.waitAsync(delay, () => setWindowPosition(this.id, newX, newY)));
+          return Promise.all(promises);
         },
         resize(width?: number, height?: number) {
           if (this.isMinimized) {
