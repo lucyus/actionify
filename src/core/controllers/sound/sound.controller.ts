5c44ef9fe75818cee206513bbb065958a864dadb
iSwitchL <55490406+iSwitchL@users.noreply.github.com>
2025-10-05 20:27:32 +0200

    feat(@core/controllers): implement Text-to-Speech (TTS)

--- src/core/controllers/sound/sound.controller.ts
+++ src/core/controllers/sound/sound.controller.ts
@@ -2,6 +2,7 @@ import path from "path";
 import { Actionify } from "../../../core";
 import {
   playSound,
+  textToSpeech,
 } from "../../../addon";
 import { SoundTrackController } from "../../../core/controllers";
 import { Inspectable } from "../../../core/utilities";
@@ -45,6 +46,36 @@ export class SoundController {
     const endTime = options?.time?.end;
     const sound = playSound(absoluteAudioPath, volume, speed, startTime, endTime);
     return new SoundTrackController(sound.id, absoluteAudioPath, volume, speed, sound.duration, startTime, endTime);
+  }
+
+  /**
+   * @description Convert text to speech (TTS) and play it.
+   *
+   * @param text The text to speak.
+   * @param options.volume The volume of the speech (0 to 100). Default is 100.
+   * @param options.speed The speed of the speech (-10 to 10). Default is 0 (normal speed).
+   * @param options.voice The voice to use for the speech. Default is your system's default voice. You can:
+   *   - Get the list of available voices by running this PowerShell command:
+   *     `Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).GetInstalledVoices() | ForEach-Object { $_.VoiceInfo | Select-Object Name, Culture, Gender, Age, Description }`
+   *   - Install additional voices on Windows by going to Settings > Time & Language > Speech > Manage voices > Add voices.
+   * @returns A promise that resolves when the speech has finished playing.
+   *
+   * ---
+   * @example
+   * // Speak text with default options
+   * Actionify.sound.say("Hello, world!");
+   * // Speak text with custom volume
+   * Actionify.sound.say("Hello, world!", { volume: 80 });
+   * // Speak text with custom speed
+   * Actionify.sound.say("Hello, world!", { speed: -2 });
+   * // Speak text with a specific voice
+   * Actionify.sound.say("Hello, world!", { voice: "Microsoft Zira Desktop" });
+   */
+  public async say(text: string, options?: { volume?: number, speed?: number, voice?: string }) {
+    const volume = options?.volume ?? 100;
+    const speed = options?.speed ?? 0;
+    const voice = options?.voice ?? "";
+    return textToSpeech(text, volume, speed, voice);
   }
 
   /**
