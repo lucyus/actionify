98d92f248194cf306996f39c922a91b6320896e5
iSwitchL <55490406+iSwitchL@users.noreply.github.com>
2026-07-14 01:20:50 +0200

    feat(@core/controllers)!: use mouse scroll counts instead of wheel deltas

--- src/core/controllers/mouse/mouse-scroll/mouse-scroll.controller.ts
+++ src/core/controllers/mouse/mouse-scroll/mouse-scroll.controller.ts
@@ -3,6 +3,7 @@ import {
   mouseWheelScrollDown,
   mouseWheelScrollUp,
 } from "../../../../addon";
+import { OperatingSystemService } from "../../../../core/services";
 import { Inspectable } from "../../../../core/utilities";
 
 /**
@@ -15,71 +16,116 @@ export class MouseScrollController {
   /**
    * @description Simulate mouse wheel scroll down.
    *
-   * @param scrollAmount Amount of wheel deltas to scroll down. If unset, the default mouse wheel scroll amount will be used.
+   * @param scrollCount Amount of down scrolls (float). If unset, defaults to `1.0` (1 scroll).
    * @param options.delay Delay in milliseconds before the mouse wheel is scrolled down.
    * @returns A promise which resolves after the mouse wheel is scrolled down.
    *
    * ---
    * @example
    *
-   * // Scroll down 120 wheel deltas (commonly 1 scroll) immediately
+   * // Scroll down once immediately
    * Actionify.mouse.scroll.down();
    *
-   * // Scroll down 240 wheel deltas (commonly 2 scrolls) immediately
-   * Actionify.mouse.scroll.down(240);
+   * // Scroll down twice immediately
+   * Actionify.mouse.scroll.down(2.0);
    *
-   * // Scroll down 120 wheel deltas (commonly 1 scroll) in 1 second
+   * // Scroll down once after 1 second
    * await Actionify.mouse.scroll.down(undefined, { delay: 1000 });
    *
-   * // Scroll down 240 wheel deltas (commonly 2 scrolls) in 1 second
-   * await Actionify.mouse.scroll.down(240, { delay: 1000 });
+   * // Scroll down twice after 1 second
+   * await Actionify.mouse.scroll.down(2.0, { delay: 1000 });
    *
-   * // Scroll down 120 wheel deltas (commonly 1 scroll) linearly over 1 second
+   * // Scroll down linearly over 1 second the length of one scroll
    * await Actionify.mouse.scroll.down(undefined, { motion: "linear", delay: 1000, steps: "auto" });
    *
-   * // Scroll down 240 wheel deltas (commonly 2 scrolls) linearly over 1 second
-   * await Actionify.mouse.scroll.down(240, { motion: "linear", delay: 1000, steps: "auto" });
+   * // Scroll down linearly over 1 second the length of two scrolls
+   * await Actionify.mouse.scroll.down(2.0, { motion: "linear", delay: 1000, steps: "auto" });
    */
-  public async down(scrollAmount?: number, options?: { steps?: number | "auto", delay?: number, motion?: "linear" }) {
+  public async down(numberOfScrolls: number = 1.0, options?: { steps?: number | "auto", delay?: number, motion?: "linear" }) {
     const steps = options?.steps === "auto" ? Infinity : Math.max(0, Math.round(options?.steps ?? 0));
     const delay = Math.max(0, Math.floor(options?.delay ?? 0));
     const motion = options?.motion ?? "linear";
-    const initialScrollAmount = 0;
-    const finalScrollAmount = scrollAmount ?? 120;
-    if (steps === 0 || delay === 0) {
-      return Actionify.time.waitAsync(delay, () => mouseWheelScrollDown(scrollAmount));
-    }
-    //Calculate the line from start to end (the shortest diagonal)
-    const deltaScroll = finalScrollAmount - initialScrollAmount;
-    // Chebyshev distance
-    const intermediateScrolls = Math.max(0, Math.abs(deltaScroll) - 1);
-    const possibleSteps = Math.min(Math.floor(delay / 16.6), steps, intermediateScrolls);
-    const preciseDelayPerScroll = delay / (possibleSteps + 1);
-    const delayPerScroll = Math.floor(preciseDelayPerScroll);
-    let accumulatedDelay = 0;
-    let accumulatedScroll = 0;
-    const promises: Promise<void>[] = [];
-    if (possibleSteps > 0) {
-      const correctionDelayOccurrence = preciseDelayPerScroll !== delayPerScroll ? Math.ceil(1 / (preciseDelayPerScroll - delayPerScroll)) : Infinity;
-      const directionScroll = Math.sign(deltaScroll);
-      const stepScroll = deltaScroll / (possibleSteps + 1);
-      switch (motion) {
-        case "linear": {
-          const intermediateScrollAmount = directionScroll !== 0 ? Math.round(stepScroll) : 0;
-          for (let offset = 1; offset < possibleSteps + 1; offset++) {
-            const correctedDelayPerPosition = delayPerScroll + (offset % correctionDelayOccurrence === 0 ? 1 : 0);
-            promises.push(Actionify.time.waitAsync(correctedDelayPerPosition + accumulatedDelay, () => mouseWheelScrollDown(intermediateScrollAmount)));
-            accumulatedDelay += correctedDelayPerPosition;
-            accumulatedScroll += intermediateScrollAmount;
-          }
-          break;
+    switch (OperatingSystemService.platform) {
+      case "win32": {
+        const windowsDefaultScrollDpi = 120;
+        const finalScrollAmount = Math.round(numberOfScrolls * windowsDefaultScrollDpi);
+        if (steps === 0 || delay === 0) {
+          return Actionify.time.waitAsync(delay, () => mouseWheelScrollDown(finalScrollAmount));
         }
-        default:
-          break;
+        //Calculate the line from start to end (the shortest diagonal)
+        const initialScrollAmount = 0;
+        const deltaScroll = finalScrollAmount - initialScrollAmount;
+        // Chebyshev distance
+        const intermediateScrolls = Math.max(0, Math.abs(deltaScroll) - 1);
+        const possibleSteps = Math.min(Math.floor(delay / 16.6), steps, intermediateScrolls);
+        const preciseDelayPerScroll = delay / (possibleSteps + 1);
+        const delayPerScroll = Math.floor(preciseDelayPerScroll);
+        let accumulatedDelay = 0;
+        let accumulatedScroll = 0;
+        const promises: Promise<void>[] = [];
+        if (possibleSteps > 0) {
+          const correctionDelayOccurrence = preciseDelayPerScroll !== delayPerScroll ? Math.ceil(1 / (preciseDelayPerScroll - delayPerScroll)) : Infinity;
+          const directionScroll = Math.sign(deltaScroll);
+          const stepScroll = deltaScroll / (possibleSteps + 1);
+          switch (motion) {
+            case "linear": {
+              const intermediateScrollAmount = directionScroll !== 0 ? Math.round(stepScroll) : 0;
+              for (let offset = 1; offset < possibleSteps + 1; offset++) {
+                const correctedDelayPerPosition = delayPerScroll + (offset % correctionDelayOccurrence === 0 ? 1 : 0);
+                promises.push(Actionify.time.waitAsync(correctedDelayPerPosition + accumulatedDelay, () => mouseWheelScrollDown(intermediateScrollAmount)));
+                accumulatedDelay += correctedDelayPerPosition;
+                accumulatedScroll += intermediateScrollAmount;
+              }
+              break;
+            }
+            default:
+              break;
+          }
+        }
+        promises.push(Actionify.time.waitAsync(delay, () => mouseWheelScrollDown(finalScrollAmount - accumulatedScroll)));
+        return Promise.all(promises);
       }
+      case "linux": {
+        const finalScrollCount = Math.round(numberOfScrolls);
+        if (steps === 0 || delay === 0) {
+          return Actionify.time.waitAsync(delay, () => mouseWheelScrollDown(finalScrollCount));
+        }
+        //Calculate the line from start to end (the shortest diagonal)
+        const initialScrollCount = 0;
+        const deltaScroll = finalScrollCount - initialScrollCount;
+        // Chebyshev distance
+        const intermediateScrolls = Math.max(0, Math.abs(deltaScroll) - 1);
+        const possibleSteps = Math.min(Math.floor(delay / 16.6), steps, intermediateScrolls);
+        const preciseDelayPerScroll = delay / (possibleSteps + 1);
+        const delayPerScroll = Math.floor(preciseDelayPerScroll);
+        let accumulatedDelay = 0;
+        let accumulatedScroll = 0;
+        const promises: Promise<void>[] = [];
+        if (possibleSteps > 0) {
+          const correctionDelayOccurrence = preciseDelayPerScroll !== delayPerScroll ? Math.ceil(1 / (preciseDelayPerScroll - delayPerScroll)) : Infinity;
+          const directionScroll = Math.sign(deltaScroll);
+          const stepScroll = deltaScroll / (possibleSteps + 1);
+          switch (motion) {
+            case "linear": {
+              const intermediateScrollCount = directionScroll !== 0 ? Math.floor(stepScroll) : 0;
+              for (let offset = 1; offset < possibleSteps + 1; offset++) {
+                const correctedDelayPerPosition = delayPerScroll + (offset % correctionDelayOccurrence === 0 ? 1 : 0);
+                promises.push(Actionify.time.waitAsync(correctedDelayPerPosition + accumulatedDelay, () => mouseWheelScrollDown(intermediateScrollCount)));
+                accumulatedDelay += correctedDelayPerPosition;
+                accumulatedScroll += intermediateScrollCount;
+              }
+              break;
+            }
+            default:
+              break;
+          }
+        }
+        promises.push(Actionify.time.waitAsync(delay, () => mouseWheelScrollDown(finalScrollCount - accumulatedScroll)));
+        return Promise.all(promises);
+      }
+      default:
+        throw new Error(`Unsupported platform: ${OperatingSystemService.platform}`);
     }
-    promises.push(Actionify.time.waitAsync(delay, () => mouseWheelScrollDown(finalScrollAmount - accumulatedScroll)));
-    return Promise.all(promises);
   }
 
   /**
@@ -92,64 +138,109 @@ export class MouseScrollController {
    * ---
    * @example
    *
-   * // Scroll up 120 wheel deltas (commonly 1 scroll) immediately
+   * // Scroll up once immediately
    * Actionify.mouse.scroll.up();
    *
-   * // Scroll up 240 wheel deltas (commonly 2 scrolls) immediately
-   * Actionify.mouse.scroll.up(240);
+   * // Scroll up twice immediately
+   * Actionify.mouse.scroll.up(2.0);
    *
-   * // Scroll up 120 wheel deltas (commonly 1 scroll) in 1 second
+   * // Scroll up once after 1 second
    * await Actionify.mouse.scroll.up(undefined, { delay: 1000 });
    *
-   * // Scroll up 240 wheel deltas (commonly 2 scrolls) in 1 second
-   * await Actionify.mouse.scroll.up(240, { delay: 1000 });
+   * // Scroll up twice after 1 second
+   * await Actionify.mouse.scroll.up(2.0, { delay: 1000 });
    *
-   * // Scroll up 120 wheel deltas (commonly 1 scroll) linearly over 1 second
+   * // Scroll up linearly over 1 second the length of one scroll
    * await Actionify.mouse.scroll.up(undefined, { motion: "linear", delay: 1000, steps: "auto" });
    *
-   * // Scroll up 240 wheel deltas (commonly 2 scrolls) linearly over 1 second
-   * await Actionify.mouse.scroll.up(240, { motion: "linear", delay: 1000, steps: "auto" });
+   * // Scroll up linearly over 1 second the length of two scrolls
+   * await Actionify.mouse.scroll.up(2.0, { motion: "linear", delay: 1000, steps: "auto" });
    */
-  public async up(scrollAmount?: number, options?: { steps?: number | "auto", delay?: number, motion?: "linear" }) {
+  public async up(numberOfScrolls: number = 1.0, options?: { steps?: number | "auto", delay?: number, motion?: "linear" }) {
     const steps = options?.steps === "auto" ? Infinity : Math.max(0, Math.round(options?.steps ?? 0));
     const delay = Math.max(0, Math.floor(options?.delay ?? 0));
     const motion = options?.motion ?? "linear";
-    const initialScrollAmount = 0;
-    const finalScrollAmount = scrollAmount ?? 120;
-    if (steps === 0 || delay === 0) {
-      return Actionify.time.waitAsync(delay, () => mouseWheelScrollUp(scrollAmount));
-    }
-    //Calculate the line from start to end (the shortest diagonal)
-    const deltaScroll = finalScrollAmount - initialScrollAmount;
-    // Chebyshev distance
-    const intermediateScrolls = Math.max(0, Math.abs(deltaScroll) - 1);
-    const possibleSteps = Math.min(Math.floor(delay / 16.6), steps, intermediateScrolls);
-    const preciseDelayPerScroll = delay / (possibleSteps + 1);
-    const delayPerScroll = Math.floor(preciseDelayPerScroll);
-    let accumulatedDelay = 0;
-    let accumulatedScroll = 0;
-    const promises: Promise<void>[] = [];
-    if (possibleSteps > 0) {
-      const correctionDelayOccurrence = preciseDelayPerScroll !== delayPerScroll ? Math.ceil(1 / (preciseDelayPerScroll - delayPerScroll)) : Infinity;
-      const directionScroll = Math.sign(deltaScroll);
-      const stepScroll = deltaScroll / (possibleSteps + 1);
-      switch (motion) {
-        case "linear": {
-          const intermediateScrollAmount = directionScroll !== 0 ? Math.round(stepScroll) : 0;
-          for (let offset = 1; offset < possibleSteps + 1; offset++) {
-            const correctedDelayPerPosition = delayPerScroll + (offset % correctionDelayOccurrence === 0 ? 1 : 0);
-            promises.push(Actionify.time.waitAsync(correctedDelayPerPosition + accumulatedDelay, () => mouseWheelScrollUp(intermediateScrollAmount)));
-            accumulatedDelay += correctedDelayPerPosition;
-            accumulatedScroll += intermediateScrollAmount;
-          }
-          break;
+    switch (OperatingSystemService.platform) {
+      case "win32": {
+        const windowsDefaultScrollDpi = 120;
+        const finalScrollAmount = Math.round(numberOfScrolls * windowsDefaultScrollDpi);
+        if (steps === 0 || delay === 0) {
+          return Actionify.time.waitAsync(delay, () => mouseWheelScrollUp(finalScrollAmount));
         }
-        default:
-          break;
+        //Calculate the line from start to end (the shortest diagonal)
+        const initialScrollAmount = 0;
+        const deltaScroll = finalScrollAmount - initialScrollAmount;
+        // Chebyshev distance
+        const intermediateScrolls = Math.max(0, Math.abs(deltaScroll) - 1);
+        const possibleSteps = Math.min(Math.floor(delay / 16.6), steps, intermediateScrolls);
+        const preciseDelayPerScroll = delay / (possibleSteps + 1);
+        const delayPerScroll = Math.floor(preciseDelayPerScroll);
+        let accumulatedDelay = 0;
+        let accumulatedScroll = 0;
+        const promises: Promise<void>[] = [];
+        if (possibleSteps > 0) {
+          const correctionDelayOccurrence = preciseDelayPerScroll !== delayPerScroll ? Math.ceil(1 / (preciseDelayPerScroll - delayPerScroll)) : Infinity;
+          const directionScroll = Math.sign(deltaScroll);
+          const stepScroll = deltaScroll / (possibleSteps + 1);
+          switch (motion) {
+            case "linear": {
+              const intermediateScrollAmount = directionScroll !== 0 ? Math.round(stepScroll) : 0;
+              for (let offset = 1; offset < possibleSteps + 1; offset++) {
+                const correctedDelayPerPosition = delayPerScroll + (offset % correctionDelayOccurrence === 0 ? 1 : 0);
+                promises.push(Actionify.time.waitAsync(correctedDelayPerPosition + accumulatedDelay, () => mouseWheelScrollUp(intermediateScrollAmount)));
+                accumulatedDelay += correctedDelayPerPosition;
+                accumulatedScroll += intermediateScrollAmount;
+              }
+              break;
+            }
+            default:
+              break;
+          }
+        }
+        promises.push(Actionify.time.waitAsync(delay, () => mouseWheelScrollUp(finalScrollAmount - accumulatedScroll)));
+        return Promise.all(promises);
       }
+      case "linux": {
+        const finalScrollCount = Math.round(numberOfScrolls);
+        if (steps === 0 || delay === 0) {
+          return Actionify.time.waitAsync(delay, () => mouseWheelScrollUp(finalScrollCount));
+        }
+        //Calculate the line from start to end (the shortest diagonal)
+        const initialScrollCount = 0;
+        const deltaScroll = finalScrollCount - initialScrollCount;
+        // Chebyshev distance
+        const intermediateScrolls = Math.max(0, Math.abs(deltaScroll) - 1);
+        const possibleSteps = Math.min(Math.floor(delay / 16.6), steps, intermediateScrolls);
+        const preciseDelayPerScroll = delay / (possibleSteps + 1);
+        const delayPerScroll = Math.floor(preciseDelayPerScroll);
+        let accumulatedDelay = 0;
+        let accumulatedScroll = 0;
+        const promises: Promise<void>[] = [];
+        if (possibleSteps > 0) {
+          const correctionDelayOccurrence = preciseDelayPerScroll !== delayPerScroll ? Math.ceil(1 / (preciseDelayPerScroll - delayPerScroll)) : Infinity;
+          const directionScroll = Math.sign(deltaScroll);
+          const stepScroll = deltaScroll / (possibleSteps + 1);
+          switch (motion) {
+            case "linear": {
+              const intermediateScrollCount = directionScroll !== 0 ? Math.floor(stepScroll) : 0;
+              for (let offset = 1; offset < possibleSteps + 1; offset++) {
+                const correctedDelayPerPosition = delayPerScroll + (offset % correctionDelayOccurrence === 0 ? 1 : 0);
+                promises.push(Actionify.time.waitAsync(correctedDelayPerPosition + accumulatedDelay, () => mouseWheelScrollUp(intermediateScrollCount)));
+                accumulatedDelay += correctedDelayPerPosition;
+                accumulatedScroll += intermediateScrollCount;
+              }
+              break;
+            }
+            default:
+              break;
+          }
+        }
+        promises.push(Actionify.time.waitAsync(delay, () => mouseWheelScrollUp(finalScrollCount - accumulatedScroll)));
+        return Promise.all(promises);
+      }
+      default:
+        throw new Error(`Unsupported platform: ${OperatingSystemService.platform}`);
     }
-    promises.push(Actionify.time.waitAsync(delay, () => mouseWheelScrollUp(finalScrollAmount - accumulatedScroll)));
-    return Promise.all(promises);
   }
 
   /**
